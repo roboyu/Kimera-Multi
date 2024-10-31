0df65ef1192d3b7cd96b7984dac45d98969a0ea3
RonghaiHe <hrhkjys@qq.com>
2024-10-31 16:53:04 +0800

    Modify based on argparse

--- examples/evo_median.py
+++ examples/evo_median.py
@@ -1,31 +1,33 @@
 import os
 import pandas as pd
-import sys
+import argparse
 
 
-def read_files_and_calculate_median(flag_single, directory, robot_name):
+def read_files_and_calculate_median(args, directory):
     data = []
-    if flag_single % 2:
+    if args.flag_single == 1:
         directory += 'distributed'
+    elif args.flag_single == 2:
+        directory += 'distributed_comm_50'
     else:
         directory += 'single'
     # Walk through all directories and subdirectories
     for root, _, files in os.walk(directory):
         for filename in files:
-            if filename[4:-4] == robot_name:  # Assuming the files are CSVs
+            if filename[4:-4] == args.robot:  # Assuming the files are CSVs
                 filepath = os.path.join(root, filename)
                 df = pd.read_csv(filepath)
 
-                if flag_single == 0:
+                if args.flag_single == 0:
                     val_ape_tran = df.iloc[-1].values[2]
-                elif flag_single == 1:
+                elif args.flag_single == 1:
                     val_ape_tran = df[df.iloc[:, 0].between(
-                        3000, 3000 + 40)].iloc[0].values[2]
-                elif flag_single == 2:
+                        args.value, args.value + 40)].iloc[0].values[2]
+                elif args.flag_single == 2:
                     val_ape_tran = df.iloc[-1].values[3]
-                elif flag_single == 3:
+                elif args.flag_single == 3:
                     val_ape_tran = df[df.iloc[:, 0].between(
-                        3000, 3000 + 40)].iloc[0].values[3]
+                        args.value, args.value + 40)].iloc[0].values[3]
                 data.append((root, val_ape_tran))
 
     # Ensure there are exactly 10 files
@@ -58,24 +60,40 @@ def read_files_and_calculate_median(flag_single, directory, robot_name):
             df = pd.read_csv(os.path.join(dir_with_median, file))
             if df.empty:
                 continue
-            val_ape_tran = df.iloc[-1].values[2]
-            val_ape_full = df.iloc[-1].values[3]
+            if args.flag_single == 0:
+                val_ape_tran = df.iloc[-1].values[2]
+                val_ape_full = df.iloc[-1].values[3]
+            else:
+                val_ape_tran = df[df.iloc[:, 0].between(
+                    args.value, args.value + 40)].iloc[0].values[2]
+                val_ape_full = df[df.iloc[:, 0].between(
+                    args.value, args.value + 40)].iloc[0].values[3]
+
+                if (df.iloc[:, 0] == 2000).any():
+                    print(
+                        f"{file[4:-4]} in 1: ape_tran: {df[df.iloc[:, 0] >= 2000].iloc[:, 2].median()}, "
+                        f"ape_full: {df[df.iloc[:, 0] >= 2000].iloc[:, 3].median()}")
+                else:
+                    print(
+                        f"{file[4:-4]} in 2: ape_tran: {df.iloc[:, 2].median()}, "
+                        f"ape_full: {df.iloc[:, 3].median()}")
             print(
                 f"{file[4:-4]}: ape_tran: {val_ape_tran}, ape_full: {val_ape_full}")
 
 
 if __name__ == "__main__":
-    """
-    How to run:
-    python evo_median.py [flag single: s or distributed: d] [robot_name] [flag tran:0 or full:1]
-    """
+    parser = argparse.ArgumentParser(
+        description="Calculate median val_ape_tran from CSV files.")
+    parser.add_argument("--flag_single", type=int, default=1, choices=[0, 1, 2, 3, 4, 5],
+                        help="Flag single: 0: single, 1: distributed, 2: distributed with comm 50, or 3,4,5: corresponding to pick ape_full")
+    parser.add_argument("--robot", default='sparkal1',
+                        help="Name of the robot")
+    parser.add_argument("--value", type=int, default=2000,
+                        help="Value to use for selection")
+    parser.add_argument("--value_end", type=int, default=10000,
+                        help="Value to use for selection as stability")
+    args = parser.parse_args()
+
     directory_path = "/media/sysu/Data/multi_robot_datasets/kimera_multi_datasets/evo_try/test_"
-    flag_single = 0
-    if len(sys.argv) > 1 and sys.argv[1] == "d":
-        flag_single = 1
-    robot_name = "sparkal1"  # Example robot name
-    if len(sys.argv) > 2 and sys.argv[2]:
-        robot_name = sys.argv[2]
-    if len(sys.argv) > 3 and sys.argv[3]:
-        flag_single |= 2
-    read_files_and_calculate_median(flag_single, directory_path, robot_name)
+
+    read_files_and_calculate_median(args, directory_path)
