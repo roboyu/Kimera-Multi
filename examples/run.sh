49ed79e30fee1c83de220523879104812ac4aab4
RonghaiHe <hrhkjys@qq.com>
2025-02-08 19:15:15 +0800

    feat: enhance example scripts and configuration for improved SLAM execution

--- examples/run.sh
+++ examples/run.sh
@@ -1,18 +1,53 @@
-#!/bin/sh
-DATA_PATH_STR="/media/sysu/Data/multi_robot_datasets/kimera_multi_datasets"
-NAME_TIME_STR="12_08"
-LOG_DIR_STR=${DATA_PATH_STR}"/log_data_"$NAME_TIME_STR
+#!/bin/bash
+
+###
+ # Copyright © 2025, Sun Yat-sen University, Guangzhou, Guangdong, 510275, All Rights Reserved
+ # @Author: Ronghai He
+ # @Date: 2024-09-02 15:59:25
+ # @LastEditors: RonghaiHe hrhkjys@qq.com
+ # @LastEditTime: 2025-01-10 18:25:06
+ # @FilePath: /src/kimera_multi/examples/run.sh
+ # @Version: 
+ # @Description: This script runs different SLAM examples based on the input argument.
+ # @Example: ./run.sh 2 12_07 | ./run.sh | ./run.sh 0
+### 
+
+# Add hash table for NAME_TIME_ to strings
+declare -A TIME2DATASET
+TIME2DATASET=(
+    ["10_14"]="campus_outdoor_10_14"
+    ["12_07"]="campus_tunnels_12_07"
+    ["12_08"]="campus_hybrid_12_08"
+)    
+
+CATKIN_WS_="/media/sysu/new_volume1/80G/sysu/herh/kimera_multi_ws"
+NAME_TIME_=${2:-"10_14"}
+DATA_PATH_="/media/sysu/Data/multi_robot_datasets/kimera_multi_datasets/"${TIME2DATASET[$NAME_TIME_]}
+LOG_DIR_=${DATA_PATH_}"/log_data_"$NAME_TIME_
 
 # If no input, Run the example
-if [ $# -eq 0 ]; then
+# ./run.sh or ./run.sh 2 or ./run.sh 2 12_07
+if [ $# -eq 0 -o "$1" = "2" ]; then
     # bash run.sh
-    echo "No input, run the example"
-    CATKIN_WS="/media/sysu/new_volume1/80G/sysu/herh/kimera_multi_ws" DATA_PATH=${DATA_PATH_STR} LOG_DIR=${LOG_DIR_STR} NAME_TIME=${NAME_TIME_STR} tmuxp load 1014-example.yaml
+    echo "No input, run the CSLAM example of " $NAME_TIME_
+    CATKIN_WS=${CATKIN_WS_} \
+    DATA_PATH=${DATA_PATH_} \
+    LOG_DIR=${LOG_DIR_} \
+    NAME_TIME=${NAME_TIME_} \
+    tmuxp load 1014-example.yaml
 else
-    if [ $1 = "0" ]; then
+    # ./run.sh 1 or ./run.sh 1 12_08
+    if [ $1 = "1" ]; then
         # bash run.sh 0
-        echo "Run the single example"
-        CATKIN_WS="/media/sysu/new_volume1/80G/sysu/herh/kimera_multi_ws" DATA_PATH=${DATA_PATH_STR} LOG_DIR=${LOG_DIR_STR} NAME_TIME=${NAME_TIME_STR} tmuxp load 1014-example_single6.yaml
+        echo "Run the single SLAM example for multi-robot of " $NAME_TIME_
+        CATKIN_WS=${CATKIN_WS_} DATA_PATH=${DATA_PATH_} LOG_DIR=${LOG_DIR_} NAME_TIME=${NAME_TIME_} tmuxp load 1014-example_single6.yaml
+    # ./run.sh 0 or ./run.sh 0 12_08
+    elif [ $1 = "0" ]; then
+        # bash run.sh 1
+        echo "Run the single example for test (single robot) of " $NAME_TIME_ 
+        CATKIN_WS=${CATKIN_WS_} DATA_PATH=${DATA_PATH_} LOG_DIR=${LOG_DIR_} NAME_TIME=${NAME_TIME_} tmuxp load 1014-example_single.yaml
+    else
+        echo "Invalid input"
     fi
 fi
 
--- examples/run_multi_times.sh
+++ examples/run_multi_times.sh
@@ -1,39 +1,64 @@
 #!/bin/bash
+###
+ # Copyright © 2025, Sun Yat-sen University, Guangzhou, Guangdong, 510275, All Rights Reserved
+ # @Author: Ronghai He
+ # @Date: 2024-10-31 12:32:52
+ # @LastEditors: RonghaiHe hrhkjys@qq.com
+ # @LastEditTime: 2025-01-19 22:55:53
+ # @FilePath: /src/kimera_multi/examples/run_multi_times.sh
+ # @Version: 
+ # @Description: 
+ # 
+### 
 
-# Define the target script and the file to check
-TARGET_SCRIPT="run.sh"
-CHECK_PYTHON_SCRIPT="evo_real_time.py"
-DIR_ORIGIN="/media/sysu/Data/multi_robot_datasets/kimera_multi_datasets/evo_try/test"
-DIR_DEST="/media/sysu/Data/multi_robot_datasets/kimera_multi_datasets/evo_try/test_distributed_comm_50/"
+declare -A TIME2DATASET
+TIME2DATASET=(
+    ["10_14"]="campus_outdoor_10_14"
+    ["12_07"]="campus_tunnels_12_07"
+    ["12_08"]="campus_hybrid_12_08"
+)  
 
-# Loop to run the target script 10 times
-for i in {5..10}
-do
-    echo "Running the target script for the $i-th time"
-    # Open another terminal named "run" and Run the target script
+# ROBOT_KIND: 1: single robot, 2: multi robot
+ROBOT_KIND=${1:-"2"}
+DATE=${2:-"12_07"}
+
+DIR_BASE="/media/sysu/Data/multi_robot_datasets/kimera_multi_datasets/evo_try/${TIME2DATASET[$DATE]}"
+
+# Set directories based on robot kind
+if [ $ROBOT_KIND = "1" ]; then
+    # ./run_multi_times.sh 1 12_08
+    echo "Running single robot"
+    TARGET_SCRIPT="run.sh 1 $DATE"
+    DIR_ORIGIN="${DIR_BASE}/test_single"
+else
+    # ./run_multi_times.sh 2 12_08
+    echo "Running multi robot"
+    TARGET_SCRIPT="run.sh 2 $DATE"
+    DIR_ORIGIN="${DIR_BASE}/test_distributed"
+fi
+
+DIR_DEST="${DIR_ORIGIN}_/"
+
+# Loop to run the target script multiple times
+for i in {1..3}; do
+    echo "Running iteration $i"
     gnome-terminal --title="run" -- /bin/bash -c "cd /media/sysu/new_volume1/80G/sysu/herh/kimera_multi_ws/src/kimera_multi/examples; bash $TARGET_SCRIPT"
-    # bash $TARGET_SCRIPT
-    sleep 5
-    # Loop to check if the Python script is running
-    while true; do
-        if [ $(pgrep -f "$CHECK_PYTHON_SCRIPT" | wc -l) -lt 2 ]; then
-            # Run some commands if the Python script is running
-            echo "Try to kill the rosnode"
-            rosnode kill -a
-            sleep 5
-            echo "Success. Now try to kill tmux session"
-            tmux kill-server
-            sleep 5
-            echo "Success. Now try to modify the test directory"
-            mv $DIR_ORIGIN $DIR_ORIGIN"$i"
-            mv $DIR_ORIGIN"$i" $DIR_DEST
-            echo "Success. Now try to restart the experiment"
-            break
-        else
-            echo "Python script is running, checking again..."
-            sleep 600
-        fi
+    
+    # Wait for processing to complete
+    sleep 600
+    
+    # Check for Python script completion and cleanup
+    while pgrep -f "evo_real_time.py" > /dev/null; do
+        echo "Waiting for processing to complete..."
+        sleep 600
     done
+    
+    # Cleanup and prepare for next iteration
+    tmux kill-server
+    mv "$DIR_ORIGIN" "${DIR_ORIGIN}${i}"
+    mv "${DIR_ORIGIN}${i}" "$DIR_DEST"
+    
+    echo "Iteration $i completed"
 done
 
-echo "Done. Go and check the results in ${DIR_DEST}!"
+echo "All iterations completed. Results are in ${DIR_DEST}"
